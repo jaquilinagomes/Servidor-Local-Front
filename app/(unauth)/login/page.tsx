49faddd123252be53787e8231b3728b415104f30
jaquilinagomes <jakscv@gmail.com>
2026-04-28 12:44:51 -0100

    components login - left  & right section

--- app/(unauth)/login/page.tsx
+++ app/(unauth)/login/page.tsx
@@ -1,40 +1,13 @@
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { LeftSection } from "@/components/login/left-section"
+import { RightSection } from "@/components/login/right-section";
+
 
 export default function LoginPage() {
     return (
     <div className="bg-gray-200 h-screen flex justify-between">
-        <div className="w-1/2 bg-red-500">
-        <Badge variant="secondary">Precision Concierce Service</Badge>
-
-        <h1>Elevate Your Standard of Service</h1>
-
-        <p>Join Servidor Local to experience a curated ecosystem connecting 
-            discerning clients with elite professional and premier companies.
-        </p>
-        <div className="flex justify-envenly w-full">
-            <Card>
-                <CardContent>
-                    <CardTitle>Vetted Network</CardTitle>
-                    <CardDescription>
-                        Exclusive acess to top-tied providers and clients.
-                    </CardDescription>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardContent>
-                    <CardTitle>Secured Plataform</CardTitle>
-                    <CardDescription>
-                        Enterprise-grade security for all your transactions and data.
-                    </CardDescription>
-                </CardContent>
-            </Card>
-        </div>
-        </div>
-
-        <div className="w-1/2 bg-blue-500">direita
+        <LeftSection/>
         
-        </div>
+        <RightSection/>
     </div>
     );
 }
